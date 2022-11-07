f6961a2eef766845da08865c38d76967ee0cc423
Rhiannon Monks <rhiannonlouisemonks@gmail.com>
2022-11-07 15:40:42 +0000

    added comma

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -25,6 +25,7 @@ function GoalInput(props) {
         <Image
           style={styles.image}
           source={require("../assets/images/goal.png")}
+          // source={"/some/path/to/image.png"}
         />
         <TextInput
           style={styles.textInput}
